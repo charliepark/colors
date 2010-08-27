5a8c9cdbff73cec778263fb31ac6e6fc65401353
Charlie Park <charlie@pearbudget.com>
2010-08-27 13:01:16 -0400

    first commit

---
+++ colors.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'chunky_png'
+require 'sinatra'
+
+get '/' do
+  redirect '/index.html'
+end
+
+get '/:hex' do
+  hex = params[:hex].sub('.png','')
+  if hex.size == 3
+    original = hex
+    triple = hex.split('')
+    hex = ''
+    triple.each {|n| hex << n*2}
+  end
+  png = ChunkyPNG::Image.new(1, 1, ChunkyPNG::Color::from_hex(hex))
+  png.metadata['Author'] = 'Charlie Park'
+  png.save('public/'+hex+'.png', :interlace => true)
+  png.save('public/'+original+'.png', :interlace => true) if original  
+  redirect '/'+hex+'.png'
+end
